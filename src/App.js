df4e7bfe3bbaa6a719608a30a6ae10ad4e8f42f7
Code Nation <codenation@Codes-MacBook-Pro.local>
2020-03-18 11:14:38 +0000

    updated to include display function and parent function

--- src/App.js
+++ src/App.js
@@ -1,15 +1,40 @@
 import React from 'react';
-import './App.css';
+import ReactDOM from 'react-dom';
 
 // function logRandom() {
 //   console.log(Math.random());
 // }
 
-function Button() {
-  const [counter, setCounter] = React.useState(5);
+function Button (props) {
+  // const handleClick = () => setCounter(counter+1);
   return (
-    <button onClick={() => setCounter(counter*2)}>{counter}</button>
-  )
+    <button onClick={props.onClickFunction}>
+      +1
+    </button>
+  );
 }
 
-export default Button;
+function Display(props) {
+  return (
+    <div>{props.message}</div>
+  );
+}
+
+// App
+
+function App () {
+  const [counter, setCounter] = React.useState(0);
+  const incrementCounter = () => setCounter(counter+1);
+  return (
+    <div>
+      <Button onClickFunction={incrementCounter}/>
+      <Display message={counter}/>
+    </div>
+  );
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root'));
+
+  // export default Button;
